b3965267b42ccf73bff98479f830fb95ff77448c
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-11-06 11:15:00 -0200

    Adicionar quarto exercício

---
+++ exercicio-04/script.js
@@ -0,0 +1,11 @@
+/*
+Crie um programa que identifique valores pares e ímpares. Esse programa deve:
+- Receber 10 valores numéricos e colocá-los em um vetor;
+- Iterar pelo vetor e definir se cada valor é par ou ímpar;
+- Mostrar ao usuário qual o valor sendo estudado e qual a definição dele;
+    Exemplo:
+        - "1 é ímpar"
+        - "38.219.830.922 é par"
+
+- Bônus: Garantir que o usuário digite números nos inputs, repetindo a pergunta caso ele digite qualquer outra coisa que não números;
+*/
