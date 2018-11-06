afa6b95f1812adc62b68d70f906a41b3655acd65
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-11-06 10:55:17 -0200

    Adicionar primeiro exercício

---
+++ exercicio-01/script.js
@@ -0,0 +1,5 @@
+/*
+Crie um programa que receba dois inputs numéricos do usuário. Esse programa deve:
+- Retornar para o usuário o resultado da soma dos dois valores, somente quando este for maior que 100;
+- Garantir que o usuário digite números nos dois inputs, repetindo a pergunta caso ele digite qualquer outra coisa que não números;
+ */
