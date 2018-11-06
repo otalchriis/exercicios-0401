af8c99ceeaa70a500a731d91990ff45da10f7e1d
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-11-06 11:07:06 -0200

    Adicionar segundo exercício

---
+++ exercicio-02/script.js
@@ -0,0 +1,19 @@
+/*
+Crie um programa que exiba o resultado da tabuada de um número digitado pelo usuário. O programa deve:
+- Receber um input numérico do usuário;
+- Questionar se o usuário quer ver o resultado de uma multiplicação específica:
+    - Se OK (quer ver a multiplicação específica):
+        - Receber um segundo input numérico do usuário;
+        - Mostrar ao usuário o resultado da multiplicação do primeiro e do segundo valor;
+            Exemplo:
+                valor1 X valor2 = resultado
+    - Se Cancelar (não quer ver a multiplicação específica):
+        - Mostrar ao usuário o resultado de todas as multiplicações, do 0 ao 10;
+            Exemplo:
+                valor X 1 = resultado
+                valor X 2 = resultado
+                valor X 3 = resultado
+                ...
+
+- Bônus: Garantir que o usuário digite números nos inputs, repetindo a pergunta caso ele digite qualquer outra coisa que não números;
+ */
