1cc6d38eb3a6539eb1f197da289a64cd54c3566d
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-11-06 11:11:01 -0200

    Adicionar terceiro exercício

---
+++ exercicio-03/script.js
@@ -0,0 +1,7 @@
+/*
+DESAFIO
+
+Crie um programa que reordene vetores. Esse programa deve:
+- Receber 10 valores de mesmo tipo (numéricos ou texto) e coloque-os em um vetor;
+- Ordene o vetor em ordem crescente e mostre o resultado ao usuário;
+*/
